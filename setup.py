93aa8e7e33a66d124d85160994d6a775eefd261f
Valentino Hudhra <v.hudhra@gmail.com>
2017-02-22 16:21:19 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='ckanext-termtranslation',
+      version=version,
+      description="Add term translations in CKAN from json files.",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Valentino Hudhra',
+      author_email='v.hudhra@gmail.com',
+      url='http://valentinohudhra.com',
+      license='',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
